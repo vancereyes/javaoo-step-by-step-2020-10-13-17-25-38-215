fa71150a54f128dd3bef3b60fb9c43fb61fef3e5
VANCE ALBERT REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESVA@OOCL.COM>
2020-10-14 21:56:32 +0800

    [Java OOP][Vance]feature - finished 10th test in prac08

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,7 +1,14 @@
 package practice08;
 
 public class Teacher extends Person {
-    public Teacher(int id, String name, int age) {
+    Klass klass;
+    public Teacher(int id, String name, int age, Klass klass) {
         super(id, name, age);
+
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
     }
 }
--- src/test/java/practice08/Practice08Test.java
+++ src/test/java/practice08/Practice08Test.java
@@ -72,13 +72,13 @@ public class Practice08Test {
         assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.");
     }
 //
-//    @Test
-//    public void should_teacher_have_name_and_age_and_class() throws Exception {
-//        Teacher tom = new Teacher(1, "Tom", 21, klass);
-//        assertThat(tom.getName()).isEqualTo("Tom");
-//        assertThat(tom.getAge()).isEqualTo(21);
-//        assertThat(tom.getKlass()).isEqualTo(klass);
-//    }
+    @Test
+    public void should_teacher_have_name_and_age_and_class() throws Exception {
+        Teacher tom = new Teacher(1, "Tom", 21, klass);
+        assertThat(tom.getName()).isEqualTo("Tom");
+        assertThat(tom.getAge()).isEqualTo(21);
+        assertThat(tom.getKlass()).isEqualTo(klass);
+    }
 //
 //    @Test
 //    public void should_teacher_introduce_itself_with_which_class_it_teaches() throws Exception {
