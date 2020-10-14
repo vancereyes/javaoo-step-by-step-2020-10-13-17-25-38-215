58a3a6a9773b46bedf8c0183b668973c0d571a65
VANCE ALBERT REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESVA@OOCL.COM>
2020-10-14 16:09:01 +0800

    [Java OOP][Vance] feature - practice04 all test created

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,25 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+
+    int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+    }
+
 }
